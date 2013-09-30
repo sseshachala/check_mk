ed7ea71bf4095fed9357997de349a4c00f949264
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-10-01 00:09:50 +0400

    change xervmon_agent to xervmon_broker

--- convert_xervmon.sh
+++ convert_xervmon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in $(git ls-files | grep check_mk | sed -e 's/\(check_mk[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/check_mk/xervmon_agent/');done
-ln -sf modules/xervmon_agent.py xervmon_agent 
-git grep -li check_mk | xargs sed -i 's/check_mk/xervmon_agent/g'
-git grep -li check-mk | xargs sed -i 's/check-mk/xervmon-agent/g'
+for file in $(git ls-files | grep check_mk | sed -e 's/\(check_mk[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/check_mk/xervmon_broker/');done
+ln -sf modules/xervmon_broker.py xervmon_broker
+git grep -li check_mk | xargs sed -i 's/check_mk/xervmon_broker/g'
+git grep -li check-mk | xargs sed -i 's/check-mk/xervmon-broker/g'
 git grep -li mk@mathias-kettner.de | xargs sed -i 's/mk@mathias-kettner\.de/info@xervmon\.com/g'
 
