05ce40abbe1e2d1f2c9e1aa0b750e15f5512a9e7
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-09-30 21:29:14 +0400

    added replace for check-mk string in convert script

--- convert_xervmon.sh
+++ convert_xervmon.sh
@@ -3,5 +3,6 @@
 for file in $(git ls-files | grep check_mk | sed -e 's/\(check_mk[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/check_mk/xervmon_agent/');done
 ln -sf xervmon_agent modules/xervmon_agent.py
 git grep -l check_mk | xargs sed -i 's/check_mk/xervmon_agent/g'
+git grep -l check-mk | xargs sed -i 's/check-mk/xervmon-agent/g'
 
 
