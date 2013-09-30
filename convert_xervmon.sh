e8733082c72f1f93d0ab2a6c056d5daca3d1a8ba
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-09-30 21:35:37 +0400

    ignore case in convert script

--- convert_xervmon.sh
+++ convert_xervmon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for file in $(git ls-files | grep check_mk | sed -e 's/\(check_mk[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/check_mk/xervmon_agent/');done
-ln -sf xervmon_agent modules/xervmon_agent.py
-git grep -l check_mk | xargs sed -i 's/check_mk/xervmon_agent/g'
-git grep -l check-mk | xargs sed -i 's/check-mk/xervmon-agent/g'
+ln -sf modules/xervmon_agent.py xervmon_agent 
+git grep -li check_mk | xargs sed -i 's/check_mk/xervmon_agent/g'
+git grep -li check-mk | xargs sed -i 's/check-mk/xervmon-agent/g'
 
 
