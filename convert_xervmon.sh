2974cb2287cf578c9cb9e584dd205326142935f5
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-09-30 21:54:50 +0400

    add maintainer to covert script

--- convert_xervmon.sh
+++ convert_xervmon.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for file in $(git ls-files | grep check_mk | sed -e 's/\(check_mk[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/check_mk/xervmon_agent/');done
+for file in $(git ls-files | grep xervmon_agent | sed -e 's/\(xervmon_agent[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/xervmon_agent/xervmon_agent/');done
 ln -sf modules/xervmon_agent.py xervmon_agent 
-git grep -li check_mk | xargs sed -i 's/check_mk/xervmon_agent/g'
-git grep -li check-mk | xargs sed -i 's/check-mk/xervmon-agent/g'
+git grep -li xervmon_agent | xargs sed -i 's/xervmon_agent/xervmon_agent/g'
+git grep -li xervmon-agent | xargs sed -i 's/xervmon-agent/xervmon-agent/g'
+git grep -li info@xervmon.com | xargs sed -i 's/mk@mathias-kettner\.de/info@xervmon\.com/g'
 
 
