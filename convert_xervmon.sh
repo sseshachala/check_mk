c51342a9a70fc7ee468fe2527c691469f525a13d
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-10-02 03:33:05 +0400

    convert script copyright test

--- convert_xervmon.sh
+++ convert_xervmon.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for file in $(git ls-files | grep xervmon_broker | sed -e 's/\(xervmon_broker[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/xervmon_broker/xervmon_broker/');done
+for file in $(git ls-files | grep check_mk | sed -e 's/\(check_mk[^/]*\).*/\1/' | uniq);do git mv $file $(echo $file | sed -e 's/check_mk/xervmon_broker/');done
 ln -sf modules/xervmon_broker.py xervmon_broker
-git grep -li xervmon_broker | xargs sed -i 's/xervmon_broker/xervmon_broker/g'
-git grep -li xervmon-broker | xargs sed -i 's/xervmon-broker/xervmon-broker/g'
-# git grep -li mk@mathias-kettner.de | xargs sed -i 's/mk@mathias-kettner\.de/info@xervmon\.com/g'
-git grep -li "# Boston, MA 02110-1301 USA." | xargs sed -i '/# +----/,/^# Boston, MA/{s/# Copyright Xervmon inc.
-# The official homepage is at http://www.xervmon.com/xervmon_broker
-/# Copyright Xervmon inc.\n# The official homepage is at http:\/\/www.xervmon.com\/xervmon_broker\n/p;d}'
+git grep -li check_mk | xargs sed -i 's/check_mk/xervmon_broker/g'
+git grep -li check-mk | xargs sed -i 's/check-mk/xervmon-broker/g'
+git grep -li mk@mathias-kettner.de | xargs sed -i 's/mk@mathias-kettner\.de/info@xervmon\.com/g'
+git grep -li "# Boston, MA 02110-1301 USA." | xargs sed -i '/# +----/,/^# Boston, MA/{s/# | Copyright Mathias Kettner 2013             mk@mathias-kettner.de |/# Copyright Xervmon inc.\n# The official homepage is at http:\/\/www.xervmon.com\/xervmon_broker\n/p;d}'
+
