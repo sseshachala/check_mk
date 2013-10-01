3a60b52dc6bbad41b0973b65ca48f91e50a1444d
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-10-02 02:01:32 +0400

    added copyright to convert script

--- convert_xervmon.sh
+++ convert_xervmon.sh
@@ -5,4 +5,5 @@ ln -sf modules/xervmon_broker.py xervmon_broker
 git grep -li check_mk | xargs sed -i 's/check_mk/xervmon_broker/g'
 git grep -li check-mk | xargs sed -i 's/check-mk/xervmon-broker/g'
 git grep -li mk@mathias-kettner.de | xargs sed -i 's/mk@mathias-kettner\.de/info@xervmon\.com/g'
+git grep -li "# Boston, MA 02110-1301 USA." | xargs sed -i '0,/^# Boston, MA/d; 1,1 s/^/# Copyright Xervmon inc.\n# The official homepage is at http:\/\/www.xervmon.com\/xervmon_broker\n /'
 
